f1c046f47d0e462a60590485e8d2288863766381
code-lokesh2008 <76845820+coder-lg@users.noreply.github.com>
2021-04-25 19:24:20 +0530

    check it out

--- app.py
+++ app.py
@@ -5,11 +5,29 @@ import os
 
 #Root
 root=tk.Tk()
+apps=[]
 
 #AddApp command definition
 def addApp():
-    filename=filedialog.askopenfilename(initialdir="/", title="Choose ya file, mate!", filetypes=(("executables","*.exe"),("all.files", "*.*")))
 
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+
+
+    filename=filedialog.askopenfilename(initialdir="/", title="Choose ya file, mate!", filetypes=(("executables","*.exe"),("all.files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label=tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 #Canvas
 canvas=tk.Canvas(root, height=500, width=500, bg="#bbb2e9")
@@ -24,8 +42,9 @@ frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 openFile=tk.Button(root, text="Open File", padx=10, pady=5, fg="black", bg="#bbb2e9", command=addApp)
 openFile.pack()
 
-RunApps=tk.Button(root, text="Run Apps", padx=10, pady=5, fg="black", bg="#bbb2e9")
+RunApps=tk.Button(root, text="Run Apps", padx=10, pady=5, fg="black", bg="#bbb2e9", command=runApps)
 RunApps.pack()
+
 
 
 
@@ -33,3 +52,12 @@ RunApps.pack()
 
 #Mainloop
 root.mainloop()
+
+with open('appsave.txt', 'w') as f:
+    for apps in apps:
+        f.write(app + ',')
+
+
+
+
+
