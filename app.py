34250d0b93504d1324a77a1e54d6a4ab7a4141cb
code-lokesh2008 <76845820+coder-lg@users.noreply.github.com>
2021-04-25 19:04:40 +0530

    Process completed till opening apps with popups

--- app.py
+++ app.py
@@ -6,9 +6,27 @@ import os
 #Root
 root=tk.Tk()
 
+#AddApp command definition
+def addApp():
+    filename=filedialog.askopenfilename(initialdir="/", title="Choose ya file, mate!", filetypes=(("executables","*.exe"),("all.files", "*.*")))
+
 
 #Canvas
-canvas=tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas=tk.Canvas(root, height=500, width=500, bg="#bbb2e9")
+canvas.pack()
+
+frame=tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+
+
+#Button
+openFile=tk.Button(root, text="Open File", padx=10, pady=5, fg="black", bg="#bbb2e9", command=addApp)
+openFile.pack()
+
+RunApps=tk.Button(root, text="Run Apps", padx=10, pady=5, fg="black", bg="#bbb2e9")
+RunApps.pack()
+
 
 
 
